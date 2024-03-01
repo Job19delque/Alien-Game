1cc5d7afd6501db8a510292df56fe4666b432c43
Job <jackvianey19@gmail.com>
2024-03-01 15:40:04 +0200

    Added Group functionality to the main file.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,11 @@
 """ This is the main file. Where you run to play the game. 
     The others file are imported here.
      """
+
 import pygame
+from pygame.sprite import (
+    Group,
+)  # This behaves like a list wit some extra functionalities
 
 from settings import Settings
 from ship import Ship
@@ -22,12 +26,18 @@ def run_game():
     # Making a ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in.
+    bullets = (
+        Group()
+    )  # To draw bullets in the screen on each pass through the main loop and to update each bullet position
+
     # Set the background color
     # This is specified only once, so define it before the main while loop.
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
         gf.check_events(ship)
         ship.update()
         gf.update_screen(ai_settings, screen, ship)

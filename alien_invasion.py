e5e8e7e6375c6265a8db13c02c74ef39d1d7ce1e
Job <jackvianey19@gmail.com>
2024-03-01 16:54:16 +0200

    Deleted some lines

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,19 +28,16 @@ def run_game():
 
     # Make a group to store bullets in.
     bullets = (
-        Group()
+        Group()  # It is outside of the main loop to avoiding creating many groups
     )  # To draw bullets in the screen on each pass through the main loop and to update each bullet position
-
-    # Set the background color
-    # This is specified only once, so define it before the main while loop.
-    bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()

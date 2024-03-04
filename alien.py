304a2e96170015a836dc97f69fede2b3b8a8c159
Job <jackvianey19@gmail.com>
2024-03-04 23:36:48 +0200

    Fixed alien position

--- alien.py
+++ alien.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
     """A class to represent a single alien in the fleet."""
 
@@ -11,5 +12,16 @@ class Alien(Sprite):
         self.ai_settings == ai_settings
 
         # Load the image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load("images/alien.bmp")
         self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact position.
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """Draw the alien at its current location."""
+        self.screen.blit(self.image, self.rect)

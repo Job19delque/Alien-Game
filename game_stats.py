f20acfba93afb1a40b6cdee4fd9fb49912e62193
Job <jackvianey19@gmail.com>
2024-03-08 02:01:43 +0200

    New file. To control the stats

---
+++ game_stats.py
@@ -0,0 +1,16 @@
+"""To count how many times the ship has been hit as well as scoring"""
+
+class GameStats():
+    """Track statistics for Alien Invasion"""
+
+    def __init__(self, ai_settings):
+        """Initialize statistics"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        # Start Alien Invasion in an active state.
+        self.game_active = True # When the player runs out of ships
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.ai_settings.ship_limit

b321d5aeb7816ed734d06cd8b925bb56e0a8a357
Job <jackvianey19@gmail.com>
2024-03-13 01:23:59 +0200

    Update func update_screen for the scoreboard

--- game_functions.py
+++ game_functions.py
@@ -61,7 +61,7 @@ def check_play_button(
         ship.center_ship()
 
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop
     screen.fill(ai_settings.bg_color)
@@ -72,6 +72,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
 
     ship.blitme()
     aliens.draw(screen)  # To make alien appear onscreen
+
+    # Draw the score information
+    sb.show_score()
 
     # Draw the play button if the game is inactive
     if not stats.game_active:

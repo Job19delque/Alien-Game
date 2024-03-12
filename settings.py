497ff3a606254e5f699d471b794bb27fca8b51c3
Job <jackvianey19@gmail.com>
2024-03-12 23:49:43 +0200

    Leveling up the speed

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
     """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings"""
+        """Initialize the game's static settings"""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -30,3 +30,8 @@ class Settings:
         self.fleet_drop_speed = 10
         # fleet_direction 0f 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()

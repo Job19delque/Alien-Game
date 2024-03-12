7c638ec30d827697cca86542b7eadf3ff9f73871
Job <jackvianey19@gmail.com>
2024-03-13 01:20:05 +0200

    Showing it to the screen

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,7 @@
 import pygame.font
 
-class Scoreboard():
+
+class Scoreboard:
     """A class to report scoring information"""
 
     def __init__(self, ai_settings, screen, stats):
@@ -16,4 +17,19 @@ class Scoreboard():
 
         # Prepare the initial score image
         self.prep_score()
-        
+
+    def pre_score(self):
+        """Turn the score into a rendered image"""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(
+            score_str, True, self.text_color, self.ai_settings.bg_color
+        )
+
+        # Display the score at the top right of the screen
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.score_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw score to the screen"""
+        self.screen.blit(self.score_image, self.score_rect)
